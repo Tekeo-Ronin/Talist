a26b776479bda8c9ca2cc74bfbe8fdc14d31842f
Volodymyr Korzhevskyi <korzhevskyiv@gmail.com>
2021-08-14 21:47:29 +0300

    Implementation of light and dark theme

--- main.js
+++ main.js
@@ -33,19 +33,89 @@ const tasks = [
     return acc;
   }, {});
 
+  const themes = {
+    default: {
+      '--base-text-color': '#212529',
+      '--header-bg': '#007bff',
+      '--header-text-color': '#fff',
+      '--default-btn-bg': '#007bff',
+      '--default-btn-text-color': '#fff',
+      '--default-btn-hover-bg': '#0069d9',
+      '--default-btn-border-color': '#0069d9',
+      '--danger-btn-bg': '#dc3545',
+      '--danger-btn-text-color': '#fff',
+      '--danger-btn-hover-bg': '#bd2130',
+      '--danger-btn-border-color': '#dc3545',
+      '--input-border-color': '#ced4da',
+      '--input-bg-color': '#fff',
+      '--input-text-color': '#495057',
+      '--input-focus-bg-color': '#fff',
+      '--input-focus-text-color': '#495057',
+      '--input-focus-border-color': '#80bdff',
+      '--input-focus-box-shadow': '0 0 0 0.2rem rgba(0, 123, 255, 0.25)',
+    },
+    dark: {
+      '--base-text-color': '#212529',
+      '--header-bg': '#343a40',
+      '--header-text-color': '#fff',
+      '--default-btn-bg': '#58616b',
+      '--default-btn-text-color': '#fff',
+      '--default-btn-hover-bg': '#292d31',
+      '--default-btn-border-color': '#343a40',
+      '--default-btn-focus-box-shadow':
+        '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+      '--danger-btn-bg': '#b52d3a',
+      '--danger-btn-text-color': '#fff',
+      '--danger-btn-hover-bg': '#88222c',
+      '--danger-btn-border-color': '#88222c',
+      '--input-border-color': '#ced4da',
+      '--input-bg-color': '#fff',
+      '--input-text-color': '#495057',
+      '--input-focus-bg-color': '#fff',
+      '--input-focus-text-color': '#495057',
+      '--input-focus-border-color': '#78818a',
+      '--input-focus-box-shadow': '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+    },
+    light: {
+      '--base-text-color': '#212529',
+      '--header-bg': '#fff',
+      '--header-text-color': '#212529',
+      '--default-btn-bg': '#fff',
+      '--default-btn-text-color': '#212529',
+      '--default-btn-hover-bg': '#e8e7e7',
+      '--default-btn-border-color': '#343a40',
+      '--default-btn-focus-box-shadow':
+        '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+      '--danger-btn-bg': '#f1b5bb',
+      '--danger-btn-text-color': '#212529',
+      '--danger-btn-hover-bg': '#ef808a',
+      '--danger-btn-border-color': '#e2818a',
+      '--input-border-color': '#ced4da',
+      '--input-bg-color': '#fff',
+      '--input-text-color': '#495057',
+      '--input-focus-bg-color': '#fff',
+      '--input-focus-text-color': '#495057',
+      '--input-focus-border-color': '#78818a',
+      '--input-focus-box-shadow': '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+    },
+  };
+
+  let lastSelectedTheme = 'default';
+
   //Elements UI
   const listContainer = document.querySelector(
     ".tasks-list-section .list-group"
   );
-
   const form = document.forms["addTask"];
   const inputTitle = form.elements["title"];
   const inputBody = form.elements["body"];
+  const themeSelect = document.getElementById('themeSelect');
 
   //Events
   renderAllTasks(objOfTasks);
   form.addEventListener("submit", onFormSubmitHandler);
-  listContainer.addEventListener('click', onDeleteHandler);
+  listContainer.addEventListener("click", onDeleteHandler);
+  themeSelect.addEventListener('change', onThemeSelectHandler);
 
   function renderAllTasks(tasksList) {
     if (!tasksList) {
@@ -71,7 +141,7 @@ const tasks = [
       "mt-2"
     );
 
-    li.setAttribute('data-task-id', _id);
+    li.setAttribute("data-task-id", _id);
 
     const span = document.createElement("span");
     span.textContent = title;
@@ -112,20 +182,20 @@ const tasks = [
     const newTask = {
       title,
       body,
-      completed: 'false',
+      completed: "false",
       _id: `task-${Math.random()}`,
-    }
+    };
 
     objOfTasks[newTask._id] = newTask;
 
-    return { ...newTask};
+    return { ...newTask };
   }
 
   function deleteTask(id) {
-    const isConfirm = confirm('You are sure?');
+    const isConfirm = confirm("You are sure?");
     if (!isConfirm) return isConfirm;
     delete objOfTasks[id];
-    return isConfirm
+    return isConfirm;
   }
 
   function deleteTaskFromHTML(confirmed, el) {
@@ -133,13 +203,30 @@ const tasks = [
     el.remove();
   }
 
-  function onDeleteHandler({target}) {
-    if (target.classList.contains('delete-btn')) {
-      const parent = target.closest('[data-task-id]');
+  function onDeleteHandler({ target }) {
+    if (target.classList.contains("delete-btn")) {
+      const parent = target.closest("[data-task-id]");
       const id = parent.dataset.taskId;
       const confirmed = deleteTask(id);
       deleteTaskFromHTML(confirmed, parent);
     }
   }
 
+  function onThemeSelectHandler(e) {
+    const selectedTheme = themeSelect.value;
+    const isConfirmed = confirm('Do you want to change the theme?');
+    if (!isConfirmed) {
+      themeSelect.value = lastSelectedTheme;
+      return;
+    };
+    setTheme(selectedTheme);
+    lastSelectedTheme = selectedTheme;
+  }
+
+  function setTheme(name) {
+    const selectedThemeObj = themes[name];
+    Object.entries(selectedThemeObj).forEach(([key, value]) => {
+      document.documentElement.style.setProperty(key, value);
+    })
+  }
 })(tasks);
